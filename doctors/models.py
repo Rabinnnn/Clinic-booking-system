2f6f2651c9bf18bc518af646f0d7dd0f8d6cf36e
rabinnnn <otiinibar@gmail.com>
2026-08-19 20:25:53 +0300

    feat: create models

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    working_hours_start = models.TimeField()   # e.g., 09:00:00
+    working_hours_end = models.TimeField()     # e.g., 17:00:00
+
+    def __str__(self):
+        return f"Dr. {self.name}"
